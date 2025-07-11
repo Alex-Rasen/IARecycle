9de4002b1d943c4e4f2b2346c916747eb886c80b
AlejandroBValgreti <150799682+Alex-Rasen@users.noreply.github.com>
2025-07-11 05:01:13 -0400

    update pwa

--- webapp/static/sw.js
+++ webapp/static/sw.js
@@ -1,2 +1,12 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('v1').then(c=>c.addAll(['/']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'v1';
+const URLS = [
+  '/',
+  '/static/manifest.json',
+  '/static/styles.css'
+];
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)))
+);
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+);
